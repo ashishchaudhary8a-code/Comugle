b6cc6939ac9b9cfe55d94575a05ca9e6d83a415a
Ashish Chaudhary <ashishchaudhary8a@gmail.com>
2026-06-30 06:35:10 +0530

    update vercel website

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const allowedOrigins = [
   "http://localhost:3000",
-  // the website on which its gonna be deployed on "https://your-app-name.netlify.app"
+  "https://comugle.vercel.app/"
 ];
 const corsOptions = {
   origin : function(origin, callback){
@@ -495,7 +495,7 @@ io.on("connection", (socket) => {
 });
 
 // ===== START SERVER =====
-const PORT = process.eventNames.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () =>{
   console.log(`🚀 Server running on ${PORT}`);
